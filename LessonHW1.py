9704fe4e80046ce792267bf59b203cb1747bad66
Roman <rom-shust@yandex.ru>
2024-07-13 14:42:19 +0400

    Add files via upload

---
+++ LessonHW1.py
@@ -0,0 +1,8 @@
+# 1st programm
+print((9**0.5)*5)
+#2nd programm
+print(9.99>9.98 and 1000!=1000.1)
+#3rd programm
+print((1234//10)%100 + ((5678//10)%100))
+#4th prohramm
+print((13.42//1)==13 or ((42.13//1==42)))
